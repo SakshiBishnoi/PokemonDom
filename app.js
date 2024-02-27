b2084a649a4bf05b5e25ef1fc37a5be5c85be618
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-02-27 16:33:35 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const container  = document.querySelector("#container");
+const newImg = document.createElement( "img" );
+newImg.src = 'https://img.freepik.com/free-vector/hand-drawn-kawaii-food-illustration_52683-84840.jpg?w=1380&t=st=1709031245~exp=1709031845~hmac=483ada493c16e51b3430af1ea658ac0145977baffc57ed84feb7637acd9445b1'
+container.appendChild(newImg);
